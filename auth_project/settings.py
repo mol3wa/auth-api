46d78551e9ee675b8d8ad4339b8a2562521cae55
Mol3wa <adebaymo@gmail.com>
2026-05-14 13:44:34 +0100

    Add missing TEMPLATES configuration

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -51,7 +51,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 ROOT_URLCONF = 'auth_project.urls'
 
